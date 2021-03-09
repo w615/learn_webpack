15e6cdd7dba4a0cc3762ba640d9e617a719bfb36
DongchuangWang <1812991192@qq.com>
2021-03-09 18:01:12 +0800

    base webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 let a = "w615";
 let b = "hello ";
 console.log(`你好,${a}--${b}`);
+console.log(333);
+import Img from "./2.jpg";
+var img = new Image();
+img.src = Img;
+document.getElementById("root").appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path"); //node path  路径
 const fs = require("fs"); // node  文件加载模块
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
   mode: "development", //指定构建模式
   entry: "./src/index.js", //入口文件
@@ -8,5 +11,47 @@ module.exports = {
     //出口文件
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, "dist"),
+    // 因为修改hostname 导致不能正常启动 添加如下可以
+    disableHostCheck: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 这里是根据这个src目录下的index.html模板创建样式
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  // 加载模块
+  module: {
+    // file-loader
+    rules: [
+      // {
+      //   test: /\.jpg$/,
+      //   use: {
+      //     loader: "file-loader",
+      //     // 其他配置项
+      //     options: {
+      //       name: "[name]_[hash].[ext]", //输出图片的名字
+      //       outputPath: "images", //文件打包位置
+      //     },
+      //   },
+      // },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "[name]_[hash].[ext]",
+            outputPath: "images/",
+            limit: "204800", // 设置文件大小超过这个值的转为base64 否则打包成图片
+          },
+        },
+      },
+      {},
+    ],
   },
 };
